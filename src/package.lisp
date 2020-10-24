24bd239b9b189da6b5766bd757504b9b6fa61602
rsrrrrrr <1536942665@qq.com>
2020-10-24 11:17:24 +0800

    vulkan bind

---
+++ src/package.lisp
@@ -0,0 +1,16 @@
+(defpackage :vkvk
+  (:use :cl :cffi))
+
+(in-package :vkvk)
+
+(define-foreign-library libvulkan
+  (:windows "vulkan-1.dll")
+  (:unix (:or "libvulkan.so")))
+
+(define-foreign-library libglfw3
+     (:unix (:or "libglfw.so.3.1" "libglfw.so.3"))
+     (:windows "glfw3.dll")
+     (t (:or (:default "libglfw3") (:default "libglfw"))))
+
+(use-foreign-library libvulkan)
+(use-foreign-library libglfw3)
