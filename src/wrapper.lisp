b93d86a2956fb0b39b6c93bec9112a45deca2120
rsrrrrrr <1536942665@qq.com>
2020-12-03 21:46:52 +0800

    update spec

--- src/wrapper.lisp
+++ src/wrapper.lisp
@@ -8,20 +8,25 @@
 
 ;;export
 (defun create-instance (create-info &optional (allocator (null-pointer)))
-  (with-foreign-objects ((info 'instance-create-info)
-			 (instance 'vk-instance))
-    (setf (mem-ref info 'instance-create-info)
+  (with-foreign-objects ((p-info 'instance-create-info)
+			 (p-instance 'vk-instance))
+    (setf (mem-ref p-info 'instance-create-info)
 	  (get-obj create-info 'instance-create-info))
-    (check-ret (vkCreateInstance info allocator instance))
+    (check-ret (vkCreateInstance p-info allocator p-instance))
     (free-objs)
-    (mem-ref instance 'vk-instance)))
+    (mem-ref p-instance 'vk-instance)))
 
 (defun destroy-instance (instance &optional (allocator (null-pointer)))
   (vkDestroyInstance instance allocator))
-#|
+
 (defun create-device (physical-device create-info &optional (allocator (null-pointer)))
-  (with-foreign-objects ((info )
-			 (device 'vk-device))))
-|#
+  (with-foreign-objects ((p-info 'device-create-info)
+			 (p-device 'vk-device))
+    (setf (mem-ref p-info 'device-create-info)
+	  (get-obj create-info 'device-create-info))
+    (check-ret (vkCreateDevice physical-device p-info allocator p-device))
+    (free-objs)
+    (mem-ref p-device 'vk-device)))
+
 (defun destroy-device (device &optional (allocator (null-pointer)))
   (vkDestroyDevice device allocator))
